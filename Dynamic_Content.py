cc68130a868f7ee21ddac9cb2551e99b3e71ea44
wywproton <137596085+wywproton@users.noreply.github.com>
2023-10-29 08:58:47 +0800

    chat.py

--- Dynamic_Content.py
+++ Dynamic_Content.py
@@ -1,17 +1,19 @@
 import PySimpleGUI as sg
 
 layout = [
-    [sg.Text('Hello, world!')],
-    [sg.Button('Update', key='update')],
-    [sg.Output(size=(30, 10))]
+    [sg.Output(size=(30, 10), key='-output-')],
+    [sg.Input( key='-text-'),sg.Button('Update', key='update')],
 ]
 
 window = sg.Window('Dynamic Content', layout)
-
+t=''
 while True:
     event, values = window.read()
     if event == 'update':
-        window['output'].update(values['text'])
+        t=t+'\n'+values['-text-']
+        print(event,values)
+        window['-output-'].update(t)
+        window['-text-'].update(value='')
     elif event is None:
         break
 
